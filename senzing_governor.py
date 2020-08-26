c7fddd691683e7e36d52a61435e0c23c16b8994e
Michael Dockter <michael@dockter.com>
2020-08-26 10:30:51 -0400

    #3 Working thread lock

--- senzing_governor.py
+++ senzing_governor.py
@@ -6,6 +6,8 @@
 
 import logging
 import time
+import os
+import threading
 
 __all__ = []
 __version__ = "1.0.0"  # See https://www.python.org/dev/peps/pep-0396/
@@ -14,6 +16,7 @@ __updated__ = '2020-08-26'
 
 SENZING_PRODUCT_ID = "5017"  # See https://github.com/Senzing/knowledge-base/blob/master/lists/senzing-product-ids.md
 log_format = '%(asctime)s %(message)s'
+
 
 class Governor:
 
@@ -28,12 +31,14 @@ class Governor:
 
         log_format = '%(asctime)s %(message)s'
         logging.basicConfig(format=log_format, level=logging.INFO)
+        logging.info("Using governor-postgresql-transaction-id Governor. Version: {0} Updated: {1}".format(__version__, __updated__))
 
         # Instance variables.
 
         self.counter = 0
-        self.stride = 500
-        self.sleep_time = 15
+        self.counter_lock = threading.Lock()
+        self.sleep_time = int(os.getenv("SENZING_GOVERNOR_SLEEP", 15))
+        self.stride = int(os.getenv("SENZING_GOVERNOR_STRIDE", 500))
 
     def govern(self, *args, **kwargs):
         """
@@ -44,15 +49,17 @@ class Governor:
 
         # Faux governance.  Replace with actual governance.
 
-        self.counter += 1
+        with self.counter_lock:
+            self.counter += 1
 
-        if self.counter % self.stride == 0:
-            logging.info("Sample Governor is sleeping {0} seconds on record {1}. Hint: {2}. Replace the Governor class with your code.".format(self.sleep_time, self.counter, self.hint))
-            time.sleep(self.sleep_time)
+            if self.counter % self.stride == 0:
+                logging.info("Sample Governor is sleeping {0} seconds on record {1}. Hint: {2}.".format(self.sleep_time, self.counter, self.hint))
+                time.sleep(self.sleep_time)
 
     def cleanup(self, *args, **kwargs):
         '''  Tasks to perform when shutting down, e.g., close DB connections '''
         return
+
 
 if __name__ == '__main__':
     pass
