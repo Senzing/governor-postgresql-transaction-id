80816da8ade7ab6eaa4e2041a99327e6c957b88d
roncewind <dad@lynntribe.net>
2022-01-05 10:57:32 -0800

    #20 added logging after 10 minutes have passed

--- senzing_governor.py
+++ senzing_governor.py
@@ -341,27 +341,32 @@ class Governor:
 
                 # Go through each database connection to determine if watermark is above high_watermark.
 
+                last_log_time = 0
+
                 for database_connection in self.database_connections.values():
                     cursor = database_connection.get("cursor")
                     database_host = database_connection.get("parsed_database_url", {}).get("host")
                     database_name = database_connection.get("parsed_database_url", {}).get("dbname")
                     watermark = self.get_current_watermark(cursor, database_name)
-                    logging.info("senzing-{0}0004I Governor is checking PostgreSQL Transaction IDs. Host: {1}; Database: {2}; Current XID: {3}; High watermark XID: {4}".format(SENZING_PRODUCT_ID, database_host, database_name, watermark, self.high_watermark))
+
+                    current_log_time = datetime.datetime.now()
+                    if (((current_log_time - last_log_time).total_seconds() / 60) > 10):
+                        logging.info("senzing-{0}0004I Governor is checking PostgreSQL Transaction IDs. Host: {1}; Database: {2}; Current XID: {3}; High watermark XID: {4}".format(SENZING_PRODUCT_ID, database_host, database_name, watermark, self.high_watermark))
+                        last_log_time = current_log_time
 
                     # When we get above the low water mark, use our wait time
                     # function to start to slow down.
 
                     old_wait_time = 0
-                    last_log_time = 0
 
                     while watermark > self.low_watermark:
                         wait_time = self.get_wait_time(watermark)
                         current_log_time = datetime.datetime.now()
                         # log a message when the wait_time changes OR if 10 minutes have passed
-                        if (wait_time != old_wait_time) or (((current_log_time - old_log_time).total_seconds() / 60) > 10):
+                        if (wait_time != old_wait_time) or (((current_log_time - last_log_time).total_seconds() / 60) > 10):
                             logging.info("senzing-{0}0005I Governor waiting {1} seconds for {2} database age(XID) to go from current value of {3} to low watermark of {4}.".format(SENZING_PRODUCT_ID, self.wait_time, database_name, watermark, self.low_watermark))
                             old_wait_time = wait_time
-                            old_log_time = current_log_time
+                            last_log_time = current_log_time
                         time.sleep(wait_time)
                         watermark = self.get_current_watermark(cursor, database_name)
 
