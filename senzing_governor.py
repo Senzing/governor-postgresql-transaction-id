bf5b4cc5d26e01c42cb727d09c0e89cf79e13905
Michael Dockter <docktermj@users.noreply.github.com>
2022-05-12 13:31:12 -0400

    #25 Migrate from master to main (#26)

--- senzing_governor.py
+++ senzing_governor.py
@@ -44,7 +44,7 @@ __version__ = "1.0.5"  # See https://www.python.org/dev/peps/pep-0396/
 __date__ = '2020-08-26'
 __updated__ = '2022-03-22'
 
-SENZING_PRODUCT_ID = "5017"  # See https://github.com/Senzing/knowledge-base/blob/master/lists/senzing-product-ids.md
+SENZING_PRODUCT_ID = "5017"  # See https://github.com/Senzing/knowledge-base/blob/main/lists/senzing-product-ids.md
 log_format = '%(asctime)s %(message)s'
 
 # Lists from https://www.ietf.org/rfc/rfc1738.txt
